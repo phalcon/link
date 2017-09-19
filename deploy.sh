74b4735a2b1a8f649823101ad23a8db1568cb3c5
Paul Scarrone <paul.scarrone@gmail.com>
2017-09-19 08:32:43 -0400

    configurable deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 echo "Preparing Deploy"
-echo "Attempting to Update Langs File"
-
 export $(cat .env | xargs)
+
+echo "Fetching and updating from $GIT_REMOTE/$GIT_HEAD"
+git fetch $GIT_REMOTE
+git checkout --force ${GIT_REMOTE}/${GIT_HEAD}
+git reset --hard ${GIT_REMOTE}/${GIT_HEAD}
+
+echo "Attempting to Update Langs File"
 $(which php) util/update_langs.php
